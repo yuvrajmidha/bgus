6ff11f3943013723b4706caf558b46bac1cacb65
Yuvraj Midha <inventive8983@gmail.com>
2022-03-15 13:17:01 +0530

    refresh works

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+const fs = require('fs');
 //requiring  basic middlewares
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
@@ -35,7 +35,38 @@ app.use('/api', router);
 app.use('/assets', express.static('frontend/out/assets'));
 app.use('/_next', express.static('frontend/out/_next'));
 
-app.use(['/:url', '/'], express.static('frontend/out'))
+app.use((req, res, next) => {
+    try {
+        if(fs.existsSync(__dirname + '/frontend/out/' + req.url + '.html')) {
+            next();
+        } else {
+            res.status(404);
+            res.sendFile(__dirname + '/frontend/out/404.html')
+        }
+    } catch (err) {
+        console.error(err);
+    }
+})
 
+app.get('/solutions/:name', (req, res) => {
+    res.sendFile(__dirname + '/frontend/out/solutions/' + req.params.name + '.html')
+})
+
+app.get('/newsroom/:name', (req, res) => {
+    res.sendFile(__dirname + '/frontend/out/newsroom/' + req.params.name + '.html')
+})
+
+app.get('/:name', (req, res) => {
+    res.sendFile(__dirname + '/frontend/out/' + req.params.name + '.html')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/frontend/out/index.html')
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.sendFile(__dirname + '/frontend/out/404.html')
+});
 
 module.exports =app 
